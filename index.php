b260808db085ee51e5c6692233d5294b78ac6ab5
kratoshalo <angelmaldonadop@hotmail.com>
2017-01-24 17:30:40 -0600

    Primer Cambio
    Agregamos index y general.css

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Prensa Animal</title>
+    <link rel="stylesheet" href="css/general.css">
+  </head>
+  <body>
+    <div class="contenedor">
+      <section class="contenedorbanner">
+        <div class="bannerchico">
+          foto chica
+        </div>
+
+        <div class="bannergrande">
+          foto grande
+        </div>
+
+        <div class="bannerchico">
+          foto chica
+        </div>
+      </section>
+
+      <header>
+        <div class="cabecera">
+          <img src="" alt="Cabecera">
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Quienes Somos</a></li>
+            <li><a href="#">Secciones</a></li>
+            <li><a href="#">Contactanos</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <section class="contenido">contenedor Principal</section>
+
+      <footer>&copy;Prensa Animal 2015.</footer>
+    </div>
+  </body>
+</html>
