73c086d3a5d47a37c18c59bfd5fd6a3a3a45ad35
kratoshalo <angelmaldonadop@hotmail.com>
2017-01-25 23:05:33 -0600

    Cambio a plataforma BootStrap

--- index.php
+++ index.php
@@ -3,41 +3,14 @@
   <head>
     <meta charset="utf-8">
     <title>Prensa Animal</title>
-    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
   </head>
   <body>
-    <div class="contenedor">
-      <section class="contenedorbanner">
-        <div class="bannerchico">
-          foto chica
-        </div>
-
-        <div class="bannergrande">
-          foto grande
-        </div>
-
-        <div class="bannerchico">
-          foto chica
-        </div>
-      </section>
-
-      <header>
-        <div class="cabecera">
-          <img src="images/logos/prensalogo.jpg" alt="Cabecera">
-        </div>
-        <nav>
-          <ul>
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Quienes Somos</a></li>
-            <li><a href="#">Secciones</a></li>
-            <li><a href="#">Contactanos</a></li>
-          </ul>
-        </nav>
-      </header>
-
-      <section class="contenido">contenedor Principal</section>
-
-      <footer>&copy;Prensa Animal 2015.</footer>
+    <div class="jumbotron">asdas</div>
+    <div class="container">
+        <h1>Landing Page</h1>
     </div>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
   </body>
 </html>
